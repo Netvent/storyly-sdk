59190c7619371da7e2c1f73d67941dd898eafc6f
hmelihf <haldunmelih@gmail.com>
2021-01-28 15:31:46 +0300

    add custom size landscape swift sample

--- ios/swift/StorylyDemo/StorylyDemo/CustomSizeViewController.swift
+++ ios/swift/StorylyDemo/StorylyDemo/CustomSizeViewController.swift
@@ -13,11 +13,13 @@ class CustomSizeViewController: UIViewController {
     @IBOutlet weak var storylyViewSmall: StorylyView!
     @IBOutlet weak var storylyViewLarge: StorylyView!
     @IBOutlet weak var storylyViewXLarge: StorylyView!
-    @IBOutlet weak var storylyViewCustom: StorylyView!
+    @IBOutlet weak var storylyViewPortrait: StorylyView!
+    @IBOutlet weak var storylyViewLandscape: StorylyView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         storylyViewSmall.storylyInit = StorylyInit(storylyId: @YOUR_APP_INSTANCE_TOKEN_FROM_DASHBOARD)
         storylyViewSmall.rootViewController = self
         //storylyViewSmall.storyGroupSize = "small"
@@ -30,13 +32,26 @@ class CustomSizeViewController: UIViewController {
         storylyViewXLarge.rootViewController = self
         //storylyViewXLarge.storyGroupSize = "xlarge"
         
-        storylyViewCustom.storylyInit = StorylyInit(storylyId: @YOUR_APP_INSTANCE_TOKEN_FROM_DASHBOARD)
-        storylyViewCustom.rootViewController = self
-        //storylyViewCustom.storyGroupSize = "custom"
-        //storylyViewCustom.storyGroupIconStyling = StoryGroupIconStyling(height: 100, width: 150, cornerRadius: 15)
-        // storylyViewCustom.storyGroupIconHeight = 100
-        // storylyViewCustom.storyGroupIconWidth = 150
-        // storylyViewCustom.storyGroupIconCornerRadius = 15
+        
+        storylyViewPortrait.storylyInit = StorylyInit(storylyId: @YOUR_APP_INSTANCE_TOKEN_FROM_DASHBOARD)
+        storylyViewPortrait.rootViewController = self
+        //storylyViewPortrait.storyGroupSize = "custom"
+        
+        //storylyViewPortrait.storyGroupIconStyling = StoryGroupIconStyling(height: 150, width: 100, cornerRadius: 15)
+        
+         //storylyViewPortrait.storyGroupIconHeight = 150
+         //storylyViewPortrait.storyGroupIconWidth = 100
+         //storylyViewPortrait.storyGroupIconCornerRadius = 15
+        
+        storylyViewLandscape.storylyInit = StorylyInit(storylyId: @YOUR_APP_INSTANCE_TOKEN_FROM_DASHBOARD)
+        storylyViewLandscape.rootViewController = self
+        //storylyViewLandscape.storyGroupSize = "custom"
+        
+        //storylyViewLandscape.storyGroupIconStyling = StoryGroupIconStyling(height: 100, width: 150, cornerRadius: 15)
+        
+        //storylyViewLandscape.storyGroupIconHeight = 100
+        //storylyViewLandscape.storyGroupIconWidth = 150
+        //storylyViewLandscape.storyGroupIconCornerRadius = 15
     }
 
 }
