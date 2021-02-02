9954b644d563d67e416098cdde3240b6d994954b
hmelihf <haldunmelih@gmail.com>
2021-02-02 11:13:40 +0300

    add custom size objc sample

---
+++ ios/objc/StorylyDemo/StorylyDemo/CustomSizeViewController.h
@@ -0,0 +1,17 @@
+//
+//  UIViewController+CustomSizeViewController.h
+//  StorylyDemo
+//
+//  Created by Haldun  Fadillioglu on 2.02.2021.
+//  Copyright © 2021 App Samurai Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (CustomSizeViewController)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/objc/StorylyDemo/StorylyDemo/CustomSizeViewController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+CustomSizeViewController.m
+//  StorylyDemo
+//
+//  Created by Haldun  Fadillioglu on 2.02.2021.
+//  Copyright © 2021 App Samurai Inc. All rights reserved.
+//
+
+#import "UIViewController+CustomSizeViewController.h"
+
+@implementation UIViewController (CustomSizeViewController)
+
+@end
