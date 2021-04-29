3102966d5bd214f97776a7cb65eb5342eae4606d
hmelihf <haldunmelihf@gmail.com>
2021-04-29 18:00:48 +0300

    add use cases ShowActivity and HideActivity

---
+++ android/kotlin/StorylyDemo/app/src/main/java/com/appsamurai/storylydemo/use_cases/HideActivity.kt
@@ -0,0 +1,79 @@
+package com.appsamurai.storylydemo.use_cases
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import com.appsamurai.storyly.StoryGroup
+import com.appsamurai.storyly.StorylyInit
+import com.appsamurai.storyly.StorylyListener
+import com.appsamurai.storyly.StorylyView
+import com.appsamurai.storylydemo.databinding.ActivityHideBinding
+
+
+class HideActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityHideBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHideBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
+
+        binding.storylyView.storylyInit = StorylyInit(STORYLY_INSTANCE_TOKEN)
+
+        binding.storylyView.storylyListener = object : StorylyListener {
+            var storylyLoaded = false
+
+            override fun storylyLoaded(
+                storylyView: StorylyView,
+                storyGroupList: List<StoryGroup>
+            ) {
+                storylyLoaded = true
+            }
+
+            override fun storylyLoadFailed(
+                storylyView: StorylyView,
+                errorMessage: String
+            ) {
+                // if cached before not hide
+                if (!storylyLoaded) {
+                    Handler(mainLooper).postDelayed({
+                        storylyHideAnimation()
+                    }, 200)
+                }
+            }
+        }
+    }
+
+    private fun storylyHideAnimation() {
+        binding.storylyView.animate().apply { duration = 500; interpolator = AccelerateInterpolator() }
+            .translationY((-binding.storylyView.height).toFloat())
+            .alpha(0.0F)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    binding.storylyView.visibility = View.GONE
+                }
+            })
+
+        AnimatorSet().apply {
+            interpolator = AccelerateInterpolator()
+            play(ValueAnimator.ofInt(binding.storylyViewHolder.height, 0).apply {
+                duration = 500
+                addUpdateListener {
+                    binding.storylyViewHolder.layoutParams.height = it.animatedValue as Int
+                    binding.storylyViewHolder.requestLayout()
+                }
+            })
+        }.start()
+    }
+}
---
+++ android/kotlin/StorylyDemo/app/src/main/java/com/appsamurai/storylydemo/use_cases/ShowActivity.kt
@@ -0,0 +1,53 @@
+package com.appsamurai.storylydemo.use_cases
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.appsamurai.storyly.StoryGroup
+import com.appsamurai.storyly.StorylyInit
+import com.appsamurai.storyly.StorylyListener
+import com.appsamurai.storyly.StorylyView
+import com.appsamurai.storylydemo.databinding.ActivityShowBinding
+
+
+class ShowActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityShowBinding
+
+    private lateinit var storylyView: StorylyView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityShowBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
+
+        storylyView = StorylyView(this)
+        storylyView.storylyInit = StorylyInit(STORYLY_INSTANCE_TOKEN)
+        storylyView.setStoryGroupTextColor(Color.BLACK)
+
+        storylyView.storylyListener = object : StorylyListener {
+            var initialLoad = true
+
+            override fun storylyLoaded(storylyView: StorylyView, storyGroupList: List<StoryGroup>) {
+                //  for not to re-animate already loaded StorylyView
+                if (initialLoad) {
+                    initialLoad = false
+
+                    Handler(mainLooper).postDelayed({
+                        storylyShowAnimation()
+                    }, 200)
+                }
+            }
+        }
+    }
+
+    private fun storylyShowAnimation() {
+        val relativeLayout = RelativeLayout(this)
+        binding.storylyViewHolder.addView(relativeLayout, 2)
+        relativeLayout.addView(storylyView)
+    }
+}
