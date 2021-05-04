8895a47ed7568db0ede09d96318fd3bb308a3fce
hmelihf <haldunmelihf@gmail.com>
2021-05-04 13:17:20 +0300

    add use cases show/hide with swift and move view controllers to folders

---
+++ ios/swift/StorylyDemo/StorylyDemo/UseCases/HideStoryly/HideStorylyViewController.swift
@@ -0,0 +1,74 @@
+//
+//  HideStorylyViewController.swift
+//  StorylyDemo
+//
+//  Created by Haldun Fadillioglu on 3.05.2021.
+//  Copyright © 2021 App Samurai Inc. All rights reserved.
+//
+
+import UIKit
+import Storyly
+
+class HideStorylyViewController: UIViewController {
+
+    @IBOutlet weak var storylyHolder: UIStackView!
+    var storylyView: StorylyView!
+    
+    var initialLoad: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let STORYLY_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
+        let STORYLY_TOKEN = "asdfasdfasd"
+        storylyView = StorylyView()
+        storylyView.storylyInit = StorylyInit(storylyId: STORYLY_TOKEN)
+        storylyView.rootViewController = self
+        storylyView.delegate = self
+        
+        addColoredViewWithConstraints(color: UIColor.red)
+        addColoredViewWithConstraints(color: UIColor.blue)
+        addStorylyViewWithConstraints()
+        addColoredViewWithConstraints(color: UIColor.green)
+        addColoredViewWithConstraints(color: UIColor.yellow)
+    }
+
+    private func addStorylyViewWithConstraints() {
+        self.storylyHolder.addArrangedSubview(self.storylyView)
+        self.storylyView.translatesAutoresizingMaskIntoConstraints = false
+        self.storylyView.leftAnchor.constraint(equalTo: self.storylyHolder.leftAnchor).isActive = true
+        self.storylyView.rightAnchor.constraint(equalTo: self.storylyHolder.rightAnchor).isActive = true
+        self.storylyView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+    
+    private func addColoredViewWithConstraints(color: UIColor) {
+        let view = UIView()
+        view.backgroundColor = color
+        storylyHolder.addArrangedSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.leftAnchor.constraint(equalTo: storylyHolder.leftAnchor).isActive = true
+        view.rightAnchor.constraint(equalTo: storylyHolder.rightAnchor).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+}
+
+
+extension HideStorylyViewController: StorylyDelegate {
+
+    func storylyLoaded(_ storylyView: Storyly.StorylyView, storyGroupList: [Storyly.StoryGroup]) {
+        initialLoad = true
+    }
+
+    func storylyLoadFailed(_ storylyView: Storyly.StorylyView, errorMessage: String) {
+        if !initialLoad {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                self.removeStorylyView()
+            }
+        }
+    }
+    
+    private func removeStorylyView() {
+        self.storylyHolder.removeArrangedSubview(self.storylyView)
+        self.storylyView.isHidden = true
+    }
+}
---
+++ ios/swift/StorylyDemo/StorylyDemo/UseCases/ShowStoryly/ShowStorylyViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ShowStorylyViewController.swift
+//  StorylyDemo
+//
+//  Created by Haldun Fadillioglu on 3.05.2021.
+//  Copyright © 2021 App Samurai Inc. All rights reserved.
+//
+
+import UIKit
+import Storyly
+
+class ShowStorylyViewController: UIViewController {
+
+    @IBOutlet weak var storylyHolder: UIStackView!
+    var storylyView: StorylyView!
+    
+    var initialLoad: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        storylyView = StorylyView()
+        storylyView.storylyInit = StorylyInit(storylyId: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40")
+        storylyView.rootViewController = self
+        storylyView.delegate = self
+        
+        addColoredViewWithConstraints(color: UIColor.red)
+        addColoredViewWithConstraints(color: UIColor.blue)
+        addColoredViewWithConstraints(color: UIColor.green)
+        addColoredViewWithConstraints(color: UIColor.yellow)
+    }
+    
+    private func addColoredViewWithConstraints(color: UIColor) {
+        let view = UIView()
+        view.backgroundColor = color
+        storylyHolder.addArrangedSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.leftAnchor.constraint(equalTo: storylyHolder.leftAnchor).isActive = true
+        view.rightAnchor.constraint(equalTo: storylyHolder.rightAnchor).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+
+}
+
+
+extension ShowStorylyViewController: StorylyDelegate {
+
+    func storylyLoaded(_ storylyView: Storyly.StorylyView,
+                       storyGroupList: [Storyly.StoryGroup]) {
+        if initialLoad {
+            initialLoad = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.addStorylyViewWithConstraints()
+            }
+        }
+    }
+
+    func storylyLoadFailed(_ storylyView: Storyly.StorylyView,
+                           errorMessage: String) {
+    }
+    
+    private func addStorylyViewWithConstraints() {
+        self.storylyHolder.insertArrangedSubview(self.storylyView, at: 2)
+        self.storylyView.translatesAutoresizingMaskIntoConstraints = false
+        self.storylyView.leftAnchor.constraint(equalTo: self.storylyHolder.leftAnchor).isActive = true
+        self.storylyView.rightAnchor.constraint(equalTo: self.storylyHolder.rightAnchor).isActive = true
+        self.storylyView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+}
