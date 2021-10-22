6535bf5b1ca4bae4e3c3758c5029e977ad815be9
Yigit <caliskanyigit@gmail.com>
2021-10-22 17:37:26 +0300

    Change programmatic to storyboard instance creation

--- ios/swift/StorylyDemo/StorylyDemo/StylingTemplates/CustomStyleViewController.swift
+++ ios/swift/StorylyDemo/StorylyDemo/StylingTemplates/CustomStyleViewController.swift
@@ -16,68 +16,30 @@ class CustomStyleViewController: UIViewController {
     private let factory3: StoryGroupViewFactory = LandscapeViewFactory()
     private let factory4: StoryGroupViewFactory = WideLandscapeViewFactory()
 
-    let customStorylyView1 = StorylyView()
-    let customStorylyView2 = StorylyView()
-    let customStorylyView3 = StorylyView()
-    let customStorylyView4 = StorylyView()
+    @IBOutlet var gradientPortraitStorylyView: UIView!
+    @IBOutlet var portraitStorylyView: UIView!
+    @IBOutlet var landscapeStorylyView: UIView!
+    @IBOutlet var wideLandscapeStorylyView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
 
         // Do any additional setup after loading the view
-        customStorylyView1.translatesAutoresizingMaskIntoConstraints = false
-
-        customStorylyView1.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
-        customStorylyView1.rootViewController = self
-        customStorylyView1.storyGroupViewFactory = factory1
-        self.view.addSubview(customStorylyView1)
-
-        customStorylyView1.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        customStorylyView1.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        if #available(iOS 11.0, *) {
-            customStorylyView1.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
-        }
-        customStorylyView1.heightAnchor.constraint(equalToConstant: 178).isActive = true
+        gradientPortraitStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        gradientPortraitStorylyView.rootViewController = self
+        gradientPortraitStorylyView.storyGroupViewFactory = factory2
         
+        portraitStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        portraitStorylyView.rootViewController = self
+        portraitStorylyView.storyGroupViewFactory = factory1
         
-        customStorylyView2.translatesAutoresizingMaskIntoConstraints = false
-
-        customStorylyView2.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
-        customStorylyView2.rootViewController = self
-        customStorylyView2.storyGroupViewFactory = factory2
-        self.view.addSubview(customStorylyView2)
-
-        customStorylyView2.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        customStorylyView2.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        customStorylyView2.topAnchor.constraint(equalTo: self.customStorylyView1.bottomAnchor, constant: 5).isActive = true
+        landscapeStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        landscapeStorylyView.rootViewController = self
+        landscapeStorylyView.storyGroupViewFactory = factory3
         
-        customStorylyView2.heightAnchor.constraint(equalToConstant: 178).isActive = true
-        
-        customStorylyView3.translatesAutoresizingMaskIntoConstraints = false
-
-        customStorylyView3.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
-        customStorylyView3.rootViewController = self
-        customStorylyView3.storyGroupViewFactory = factory3
-        self.view.addSubview(customStorylyView3)
-
-        customStorylyView3.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        customStorylyView3.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        customStorylyView3.topAnchor.constraint(equalTo: self.customStorylyView2.bottomAnchor, constant: 5).isActive = true
-        
-        customStorylyView3.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        customStorylyView4.translatesAutoresizingMaskIntoConstraints = false
-
-        customStorylyView4.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
-        customStorylyView4.rootViewController = self
-        customStorylyView4.storyGroupViewFactory = factory4
-        self.view.addSubview(customStorylyView4)
-
-        customStorylyView4.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        customStorylyView4.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        customStorylyView4.topAnchor.constraint(equalTo: self.customStorylyView3.bottomAnchor, constant: 5).isActive = true
-        
-        customStorylyView4.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        wideLandscapeStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        wideLandscapeStorylyView.rootViewController = self
+        wideLandscapeStorylyView.storyGroupViewFactory = factory4
     }
 }
