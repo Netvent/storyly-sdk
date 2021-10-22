41be8e137563b7dc3d1aa37285c526361b355ba6
levent <lewentoral@gmail.com>
2021-10-22 18:08:54 +0300

    rename fields

--- ios/swift/StorylyDemo/StorylyDemo/StylingTemplates/CustomStyleViewController.swift
+++ ios/swift/StorylyDemo/StorylyDemo/StylingTemplates/CustomStyleViewController.swift
@@ -11,15 +11,15 @@ import Storyly
 
 class CustomStyleViewController: UIViewController {
     
-    private let factory1: StoryGroupViewFactory = PortraitViewFactory()
-    private let factory2: StoryGroupViewFactory = GradientPortraitViewFactory()
-    private let factory3: StoryGroupViewFactory = LandscapeViewFactory()
-    private let factory4: StoryGroupViewFactory = WideLandscapeViewFactory()
+    private let gradientPortraitViewFactory: StoryGroupViewFactory = GradientPortraitViewFactory()
+    private let portraitViewFactory: StoryGroupViewFactory = PortraitViewFactory()
+    private let landspaceViewFactory: StoryGroupViewFactory = LandscapeViewFactory()
+    private let wideLandspaceViewFactory: StoryGroupViewFactory = WideLandscapeViewFactory()
 
-    @IBOutlet var gradientPortraitStorylyView: UIView!
-    @IBOutlet var portraitStorylyView: UIView!
-    @IBOutlet var landscapeStorylyView: UIView!
-    @IBOutlet var wideLandscapeStorylyView: UIView!
+    @IBOutlet var gradientPortraitStorylyView: StorylyView!
+    @IBOutlet var portraitStorylyView: StorylyView!
+    @IBOutlet var landscapeStorylyView: StorylyView!
+    @IBOutlet var wideLandscapeStorylyView: StorylyView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,18 +28,18 @@ class CustomStyleViewController: UIViewController {
         // Do any additional setup after loading the view
         gradientPortraitStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
         gradientPortraitStorylyView.rootViewController = self
-        gradientPortraitStorylyView.storyGroupViewFactory = factory2
+        gradientPortraitStorylyView.storyGroupViewFactory = GradientPortraitViewFactory()
         
         portraitStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
         portraitStorylyView.rootViewController = self
-        portraitStorylyView.storyGroupViewFactory = factory1
+        portraitStorylyView.storyGroupViewFactory = portraitViewFactory
         
         landscapeStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
         landscapeStorylyView.rootViewController = self
-        landscapeStorylyView.storyGroupViewFactory = factory3
+        landscapeStorylyView.storyGroupViewFactory = landspaceViewFactory
         
         wideLandscapeStorylyView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
         wideLandscapeStorylyView.rootViewController = self
-        wideLandscapeStorylyView.storyGroupViewFactory = factory4
+        wideLandscapeStorylyView.storyGroupViewFactory = wideLandspaceViewFactory
     }
 }
