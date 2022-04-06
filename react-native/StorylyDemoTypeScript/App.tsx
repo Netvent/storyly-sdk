16d7d5edadae08d4b1864bacec268f27345fd350
levent <lewentoral@gmail.com>
2022-04-06 15:30:13 +0300

    update typescript sample to 1.20.1
    add sample for onLoad event usage with externalData

--- react-native/StorylyDemoTypeScript/App.tsx
+++ react-native/StorylyDemoTypeScript/App.tsx
@@ -33,8 +33,25 @@ const App = () => {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Storyly
         style={{ height: "100%" }}
-        storylyId="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
-        onLoad={event => { console.log("[Storyly] onLoad"); }}
+        ref={ref => { ref?.setExternalData(
+          [
+            {
+              "{Title}": "306",
+              "{Price}": "1000",
+              "{Image_url}": "https://img.yad2.co.il/Pic/202202/20/1_1/o/y2_1_03059_20220220170310.jpeg",
+              "{Button}": "למודעה",
+              "{Button_url}": "ylbgocfu",
+              "{sg1_image_url}": "https://img.yad2.co.il/Pic/202202/20/1_1/o/y2_1_03059_20220220170310.jpeg",
+              "{sg1_price}": "1000",
+              "{sg1_title}": "306",
+              "{sg1_subtitle}": "2000",
+              "{sg1_button}": "ylbgocfu",
+              "{sg1_button_url}": "ylbgocfu",
+            }
+          ]
+        );}}
+        storylyId="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjQ4MTgsImFwcF9pZCI6OTk4OCwiaW5zX2lkIjoxMDUxNX0.L__nUAJ1Q8wt6KApQ_KtiqEPxvftoro_RDyRSHJmCl4"
+        onLoad={event => { console.log(event); }}
         onFail={event => { console.log("[Storyly] onFail"); }}
         onPress={event => { console.log("[Storyly] onPress"); }}
         onEvent={event => { console.log("[Storyly] onEvent"); }}
