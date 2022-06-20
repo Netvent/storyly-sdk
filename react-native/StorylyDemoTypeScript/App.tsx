ce8c8ec19f184c3872e0ea8d27aebaee23b91692
levent <lewentoral@gmail.com>
2022-06-20 16:11:08 +0300

    create react-navigation sample

--- react-native/StorylyDemoTypeScript/App.tsx
+++ react-native/StorylyDemoTypeScript/App.tsx
@@ -8,42 +8,90 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { Storyly } from 'storyly-react-native';
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Storyly
-        style={{ height: "100%" }}
-        storylyId="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
-        onLoad={event => { console.log("[Storyly] onLoad"); }}
-        onFail={event => { console.log("[Storyly] onFail"); }}
-        onPress={event => { console.log("[Storyly] onPress"); }}
-        onEvent={event => { console.log("[Storyly] onEvent"); }}
-        onStoryOpen={() => { console.log("[Storyly] onStoryOpen"); }}
-        onStoryClose={() => { console.log("[Storyly] onStoryClose"); }}
-        onUserInteracted={event => { console.log("[Storyly] onUserInteracted"); }}
-      />
-    </SafeAreaView>
-  );
-};
-
-export default App;
+ import React from 'react';
+ import {
+   Button,
+   View,
+ } from 'react-native';
+ 
+ import { Storyly } from 'storyly-react-native';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ 
+ function HomeScreen({ navigation }) {
+   console.log("HomeScreen:Create");
+   return (
+     <View style={{ flex: 1 }}>
+       <Button
+         title="Go to Profile"
+         onPress={() => navigation.navigate('Profile')} 
+       />
+       <Storyly
+         style={{ height: "100%" }}
+         storylyId="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
+         onLoad={event => { console.log("[Storyly] onLoad"); }}
+         onFail={event => { console.log("[Storyly] onFail"); }}
+         onPress={event => { console.log("[Storyly] onPress"); }}
+         onEvent={event => { console.log("[Storyly] onEvent"); }}
+         onStoryOpen={() => { console.log("[Storyly] onStoryOpen"); }}
+         onStoryClose={() => { console.log("[Storyly] onStoryClose"); }}
+         onUserInteracted={event => { console.log("[Storyly] onUserInteracted"); }} 
+       />
+     </View>
+   );
+ }
+ 
+ function ProfileScreen({ navigation }) {
+   return (
+     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+       <Button
+         title="Go to Notifications"
+         onPress={() => navigation.navigate('Notifications')}
+       />
+       <Button title="Go back" onPress={() => navigation.goBack()} />
+     </View>
+   );
+ }
+ 
+ function NotificationsScreen({ navigation }) {
+   return (
+     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+       <Button
+         title="Go to Settings"
+         onPress={() => navigation.navigate('Settings')}
+       />
+       <Button title="Go back" onPress={() => navigation.goBack()} />
+     </View>
+   );
+ }
+ 
+ function SettingsScreen({ navigation }) {
+   return (
+     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+       <Button title="Go back" onPress={() => navigation.goBack()} />
+     </View>
+   );
+ }
+ 
+ const Stack = createNativeStackNavigator();
+ 
+ function MyStack() {
+   return (
+     <Stack.Navigator>
+       <Stack.Screen name="Home" component={HomeScreen}/>
+       <Stack.Screen name="Notifications" component={NotificationsScreen} />
+       <Stack.Screen name="Profile" component={ProfileScreen} />
+       <Stack.Screen name="Settings" component={SettingsScreen} />
+     </Stack.Navigator>
+   );
+ }
+ 
+ const App = () => {
+   return (
+     <NavigationContainer>
+       <MyStack />
+     </NavigationContainer>
+   );
+ }
+ 
+ export default App;
