345c3aa603026b62e4154c4994fe834d634ab40f
levent <lewentoral@gmail.com>
2022-09-15 01:59:12 +0300

    moments: update react-native version to 0.0.2
    add StorylyMomentsUserPayload class and encryptUserPayload function

--- moments/react-native/storyly-moments-react-native/android/src/main/java/com/appsamurai/storyly/moments/reactnative/STStorylyMomentsModule.kt
+++ moments/react-native/storyly-moments-react-native/android/src/main/java/com/appsamurai/storyly/moments/reactnative/STStorylyMomentsModule.kt
@@ -5,10 +5,8 @@ import android.os.Handler
 import android.os.Looper
 import com.appsamurai.storyly.moments.Config
 import com.appsamurai.storyly.moments.StorylyMomentsManager
-import com.appsamurai.storyly.moments.data.entity.MomentsStoryGroup
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
+import com.appsamurai.storyly.moments.data.entity.MomentsUserPayload
+import com.facebook.react.bridge.*
 
 class STStorylyMomentsModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -42,6 +40,41 @@ class STStorylyMomentsModule(reactContext: ReactApplicationContext) : ReactConte
   @ReactMethod
   fun openStoryCreator() {
     Handler(Looper.getMainLooper()).post { storylyMomentsManager?.createStory() }
+  }
+
+  @ReactMethod
+  fun encryptUserPayload(
+    secretKey: String,
+    initializationVector: String,
+    id: String,
+    username: String,
+    avatarUrl: String,
+    followings: ReadableArray,
+    creatorTags: ReadableArray?,
+    consumerTags: ReadableArray?,
+    expirationTime: Int,
+    promise: Promise,
+  ) {
+    promise.resolve(
+      MomentsUserPayload(
+        id = id,
+        username = username,
+        avatarUrl = avatarUrl,
+        followings = convertReadableArray(followings),
+        creatorTags = if (creatorTags != null) convertReadableArray(creatorTags) else null,
+        consumerTags = if (consumerTags != null) convertReadableArray(consumerTags) else null,
+        expirationTime = expirationTime
+      ).encryptUserPayload(
+        secretKey = secretKey,
+        initializationVector = initializationVector
+      )
+    )
+  }
+
+  private fun convertReadableArray(stringArray: ReadableArray): List<String> {
+    val stringList = mutableListOf<String?>()
+    for (i in 0 until stringArray.size()) stringList.add(stringArray.getString(i))
+    return stringList.filterNotNull()
   }
 
   private fun getActivityContext(): Context {
