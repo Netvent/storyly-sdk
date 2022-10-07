8b78459f45c585c9291869c747195d8bdf1f032f
hmelihf <melih@appsamurai.com>
2022-10-07 16:44:03 +0300

    refactor(monetization-rn): refactor monetization react-native example app

--- monetization/react-native/storyly-monetization-react-native/example/src/App.tsx
+++ monetization/react-native/storyly-monetization-react-native/example/src/App.tsx
@@ -1,24 +1,23 @@
 import * as React from 'react';
+import { useRef } from 'react';
 
 import { StyleSheet, View, Button, SafeAreaView } from 'react-native';
-import { setStorylyAdViewProvider, StorylyAdViewProvider } from 'storyly-monetization-react-native';
+import { setStorylyAdViewProvider } from 'storyly-monetization-react-native';
 import { Storyly } from 'storyly-react-native';
 
 
 export default function App() {
-  var storyly: Storyly|null;
-  
+  const storyly = useRef<Storyly>(null)
+
   return (
     <SafeAreaView style={styles.container}>
        <Storyly
-          ref={ref => {
-            storyly = ref
-            // if (ref) { setStorylyAdViewProvider(ref, "ca-app-pub-3940256099942544/2247696110") }
-          }}
+          ref={storyly}
           style={{ width: '100%', height: 120, marginTop: 44 }}
           storylyId="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjU1NiwiYXBwX2lkIjoxMzg5LCJpbnNfaWQiOjE0Mjd9.cGTn_uElzFerKU-ul3EnrTn7pMZlhA3HvG4EEoygDcQ"
           onLoad={storyGroupList => {
               console.log("[Storyly] onLoad");
+              setStorylyAdViewProvider(storyly.current, {adMobAdUnitId: "ca-app-pub-3940256099942544/2247696110", adMobAdExtras: {"npa": "1", "aa": {"test": "a"}}}) 
           }}
           onFail={errorMessage => {
               console.log("[Storyly] onFail");
@@ -41,7 +40,7 @@ export default function App() {
       <View style={styles.box}>
         <Button 
           onPress={() => {
-            if (storyly) { setStorylyAdViewProvider(storyly, {adMobAdUnitId: "ca-app-pub-3940256099942544/2247696110", adMobAdExtras: {"npa": "1", "aa": {"test": "a"}}})  } 
+            if (storyly) { setStorylyAdViewProvider(storyly.current, {adMobAdUnitId: "ca-app-pub-3940256099942544/2247696110", adMobAdExtras: {"npa": "1", "aa": {"test": "a"}}})  } 
           }}
           title="Add Ad Provider To Storyly"
           color="#7A4BFF"
@@ -50,7 +49,7 @@ export default function App() {
       <View style={styles.box}>
         <Button 
           onPress={() => {
-            if (storyly) { setStorylyAdViewProvider(storyly,  {adMobAdUnitId: "ca-app-pub-3940256099942544/2521693316"}) }
+            if (storyly) { setStorylyAdViewProvider(storyly.current,  {adMobAdUnitId: "ca-app-pub-3940256099942544/2521693316"}) }
           }}
           title="Add Video Ad Provider To Storyly"
           color="#7A4BFF"
@@ -59,7 +58,7 @@ export default function App() {
       <View style={styles.box}>
         <Button 
           onPress={() => {
-            if (storyly) { setStorylyAdViewProvider(storyly,  null) }
+            if (storyly) { setStorylyAdViewProvider(storyly.current,  null) }
           }}
           title="Remove Ad Provider From Storyly"
           color="#7A4BFF"
