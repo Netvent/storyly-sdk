3fc955c978cc3ad78f199ddc707c1dbb838ec61a
levent <lewentoral@gmail.com>
2023-04-04 12:24:13 +0300

    build(storyly-react-native): release 1.32.3

--- react-native/storyly-react-native/ios/RCTConvert+Storyly.swift
+++ react-native/storyly-react-native/ios/RCTConvert+Storyly.swift
@@ -25,37 +25,37 @@ extension RCTConvert {
         )
     }
 
-    @objc(stStoryGroupListStyling:)
-    static func stStoryGroupListStyling(json: NSDictionary) -> StoryGroupListStyling {
-        print("STR:RCTConvert+Extension:stStoryGroupListStyling:1:(json:\(json))")
-        var orientation: StoryGroupListOrientation
-        switch json["orientation"] as? String {
-            case "horizontal": orientation = .Horizontal
-            case "vertical": orientation = .Vertical
-            default: orientation = .Horizontal
-        }
-
-        print("STR:RCTConvert+Extension:stStoryGroupListStyling:2:(json:\(json))")
-        let sections = (json["sections"] as? Int) ?? 1
-        let horizontalEdgePadding = (json["horizontalEdgePadding"] as? CGFloat) ?? 4
-        let verticalEdgePadding = (json["verticalEdgePadding"] as? CGFloat) ?? 4
-        let horizontalPaddingBetweenItems = (json["horizontalPaddingBetweenItems"] as? CGFloat) ?? 8
-        let verticalPaddingBetweenItems = (json["verticalPaddingBetweenItems"] as? CGFloat) ?? 8
-
-        
-        print("STR:RCTConvert+Extension:stStoryGroupListStyling:3:(json:\(json))")
-        let storylyGroupListStyling = StoryGroupListStyling(
-            orientation: orientation,
-            sections: sections,
-            horizontalEdgePadding: horizontalEdgePadding,
-            verticalEdgePadding: verticalEdgePadding,
-            horizontalPaddingBetweenItems: horizontalPaddingBetweenItems,
-            verticalPaddingBetweenItems: verticalPaddingBetweenItems
-        )
-        print("STR:RCTConvert+Extension:stStoryGroupListStyling:4:(json:\(json))")
-        return storylyGroupListStyling
-    }
-    
+//    @objc(stStoryGroupListStyling:)
+//    static func stStoryGroupListStyling(json: NSDictionary) -> StoryGroupListStyling {
+//        print("STR:RCTConvert+Extension:stStoryGroupListStyling:1:(json:\(json))")
+//        var orientation: StoryGroupListOrientation
+//        switch json["orientation"] as? String {
+//            case "horizontal": orientation = .Horizontal
+//            case "vertical": orientation = .Vertical
+//            default: orientation = .Horizontal
+//        }
+//
+//        print("STR:RCTConvert+Extension:stStoryGroupListStyling:2:(json:\(json))")
+//        let sections = (json["sections"] as? Int) ?? 1
+//        let horizontalEdgePadding = (json["horizontalEdgePadding"] as? CGFloat) ?? 4
+//        let verticalEdgePadding = (json["verticalEdgePadding"] as? CGFloat) ?? 4
+//        let horizontalPaddingBetweenItems = (json["horizontalPaddingBetweenItems"] as? CGFloat) ?? 8
+//        let verticalPaddingBetweenItems = (json["verticalPaddingBetweenItems"] as? CGFloat) ?? 8
+//
+//        
+//        print("STR:RCTConvert+Extension:stStoryGroupListStyling:3:(json:\(json))")
+//        let storylyGroupListStyling = StoryGroupListStyling(
+//            orientation: orientation,
+//            sections: sections,
+//            horizontalEdgePadding: horizontalEdgePadding,
+//            verticalEdgePadding: verticalEdgePadding,
+//            horizontalPaddingBetweenItems: horizontalPaddingBetweenItems,
+//            verticalPaddingBetweenItems: verticalPaddingBetweenItems
+//        )
+//        print("STR:RCTConvert+Extension:stStoryGroupListStyling:4:(json:\(json))")
+//        return storylyGroupListStyling
+//    }
+//    
     @objc(stStoryGroupIconStyling:)
     static func stStoryGroupIconStyling(json: NSDictionary) -> StoryGroupIconStyling {
         print("STR:RCTConvert+Extension:stStoryGroupIconStyling(json:\(json))")
