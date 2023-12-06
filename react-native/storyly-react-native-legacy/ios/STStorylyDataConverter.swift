eee93243a0556305ce88c7bd118cf7214f716a52
hmelihf <melih@appsamurai.com>
2023-12-06 13:49:42 +0300

    feat(sync): revert legacy merge

--- react-native/storyly-react-native-legacy/ios/STStorylyDataConverter.swift
+++ react-native/storyly-react-native-legacy/ios/STStorylyDataConverter.swift
@@ -144,9 +144,8 @@ internal func createSTRProductItem(productItem: NSDictionary?) -> STRProductItem
         price: Float((productItem?["price"] as? Double) ?? 0.0),
         salesPrice: (productItem?["salesPrice"] as? NSNumber),
         currency: productItem?["currency"] as? String ?? "",
-        imageUrls: productItem?["imageUrls"] as? [String],
-        variants: createSTRProductVariant(variants: productItem?["variants"] as? [NSDictionary] ?? []),
-        ctaText: productItem?["ctaText"] as? String
+        imageUrls:productItem?["imageUrls"] as? [String],
+        variants:  createSTRProductVariant(variants: productItem?["variants"] as? [NSDictionary] ?? [])
     )
 }
 
