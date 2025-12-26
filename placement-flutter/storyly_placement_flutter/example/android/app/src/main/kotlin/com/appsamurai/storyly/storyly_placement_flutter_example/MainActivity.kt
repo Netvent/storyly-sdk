0c08c77fcb387e63728f61b8e16877aef52059e5
hmelihf <haldunmelih@gmail.com>
2025-12-26 15:02:50 +0300

    feat(str-v2-flutter): create base project

---
+++ placement-flutter/storyly_placement_flutter/example/android/app/src/main/kotlin/com/appsamurai/storyly/storyly_placement_flutter_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.appsamurai.storyly.storyly_placement_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ placement-flutter/storyly_placement_flutter/example/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'placement_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Map<String, String>> _tabs = [
+    // {
+    //   'label': 'Story Bar',
+    //   'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NfaWQiOjIzODAsImFwcF9pZCI6MTcxODUsImluc19pZCI6MTkxMDB9.AmtkzTlj_g3RQwwHZTz6rsozH8VFqAogeSwgBdXLMDU'
+    // },
+    {
+      'label': 'Video Feed',
+      'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6MjQwNDIsInQiOjF9.Uj9rEBowMUOP4zqueJQ8stXJXHdFOKoac8sKUEM8K5M'
+    },
+    {
+      'label': 'Banner',
+      'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NfaWQiOjE0ODY3LCJhcHBfaWQiOjIyNDgyLCJwbGNtbnRfaWQiOjI1NDcwLCJzZGtfcGwiOiJpb3MifQ.jAIGPCEy1GES5WQMzjqlWKj_LuPLkkLAtsTdWmwF0MM'
+    },
+    {
+      'label': 'Swipe Card',
+      'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NfaWQiOjE0ODY3LCJhcHBfaWQiOjIyNDgyLCJwbGNtbnRfaWQiOjI1NDcxLCJzZGtfcGwiOiJpb3MifQ.MeIfgCW71K0LweUH2_b16ODoPFp0MX5dQUer08SrI5k'
+    },
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tabs[_selectedIndex]['label']!),
+      ),
+      body: PlacementScreen(
+        // Key is important to force rebuild when tab changes
+        key: ValueKey(_tabs[_selectedIndex]['label']), 
+        name: _tabs[_selectedIndex]['label']!,
+        token: _tabs[_selectedIndex]['token']!,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _tabs.map((tab) {
+          return BottomNavigationBarItem(
+            icon: const Icon(Icons.ad_units), // Placeholder icon
+            label: tab['label'],
+          );
+        }).toList(),
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
