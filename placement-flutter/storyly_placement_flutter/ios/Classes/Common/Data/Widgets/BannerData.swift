0470743823f3d9f4bd9c42b5e0c394cabbb309f3
hmelihf <haldunmelih@gmail.com>
2025-12-26 15:04:41 +0300

    feat(str-v2-flutter): add common placement bridge for ios

---
+++ placement-flutter/storyly_placement_flutter/ios/Classes/Common/Data/Widgets/BannerData.swift
@@ -0,0 +1,59 @@
+import Foundation
+import StorylyCore
+import StorylyBanner
+
+// MARK: - Banner Data Payload Encoding
+
+func encodeBannerDataPayload(_ data: BannerDataPayload) -> [String: Any] {
+    return [
+        "type": STRWidgetType.banner.description,
+        "items": data.items.compactMap { encodeBannerSlide($0) }
+    ]
+}
+
+func encodeBannerPayload(_ payload: BannerPayload?) -> [String: Any]? {
+    guard let payload = payload else { return nil }
+    
+    return [
+        "component": encodeBannerComponent(payload.component),
+        "item": encodeBannerSlide(payload.item)
+    ].compactMapValues { $0 }
+}
+
+// MARK: - Banner Slide Encoding
+
+func encodeBannerSlide(_ slide: BannerSlide?) -> [String: Any]? {
+    guard let slide = slide else { return nil }
+    
+    return ([
+        "id": slide.uniqueId,
+        "name": slide.name,
+        "index": slide.index,
+        "actionUrl": slide.actionUrl,
+        "componentList": slide.componentList?.compactMap { encodeBannerComponent($0) },
+        "actionProducts": slide.actionProducts?.map { encodeSTRProductItem($0) },
+        "currentTime": slide.currentTime
+    ]  as [String: Any?]).compactMapValues { $0 }
+}
+
+// MARK: - Banner Component Encoding
+
+func encodeBannerComponent(_ component: BannerComponent?) -> [String: Any]? {
+    guard let component = component else { return nil }
+    
+    var result: [String: Any?] = [
+        "id": component.id,
+        "type": component.type.get(),
+        "customPayload": component.customPayload,
+    ]
+    
+    if let button = component as? BannerButtonComponent {
+        result["text"] = button.text
+        result["actionUrl"] = button.actionUrl
+        result["products"] = button.products?.map { encodeSTRProductItem($0) }
+    }
+    
+    return result.compactMapValues { $0 }
+}
+
+
---
+++ placement-flutter/storyly_placement_flutter/ios/Classes/Common/RNPlacementProviderEventType.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+@objc public enum RNPlacementProviderEventType: Int {
+    case onLoad
+    case onLoadFail
+    case onHydration
+    
+    public var eventName: String {
+        switch self {
+        case .onLoad:
+            return "onLoad"
+        case .onLoadFail:
+            return "onLoadFail"
+        case .onHydration:
+            return "onHydration"
+        }
+    }
+  
+    static var allCases: [RNPlacementProviderEventType] = [.onLoad, .onLoadFail, .onHydration]
+}
+
